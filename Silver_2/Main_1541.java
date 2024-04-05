d276f77a9ae3f8d0f83cf180178d703adcb0746a
wwwaas0 <127373635+wwwaas0@users.noreply.github.com>
2024-04-05 13:36:46 +0900

    [2024-04-05] 잃어버린 괄호 refactor

--- Silver_2/Main_1541.java
+++ Silver_2/Main_1541.java
@@ -8,20 +8,19 @@ public class Main_1541 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] strNumbers = br.readLine().split("-");
-        int[] intNumbers = new int[strNumbers.length];
+
+        int answer = 0;
         for (int i = 0; i < strNumbers.length; i++) {
             String[] elements = strNumbers[i].split("\\+");
             int sum = 0;
             for (String element : elements) {
                 sum += Integer.parseInt(element);
             }
-            intNumbers[i] = sum;
+
+            if(i==0) answer = sum;
+            else answer -= sum;
         }
 
-        int answer = intNumbers[0];
-        for (int i = 1; i < intNumbers.length; i++) {
-            answer -= intNumbers[i];
-        }
         System.out.println(answer);
     }
 }
